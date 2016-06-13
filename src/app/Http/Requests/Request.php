7c4cdfd5ed63d04d4dcd2632c8820545dadb7f8f
ivcandela <ivankcandela@gmail.com>
2016-06-13 10:55:31 +0200

    added JsonApiResponses behaviour to base FormRequest

--- src/app/Http/Requests/Request.php
+++ src/app/Http/Requests/Request.php
@@ -3,8 +3,41 @@
 namespace Zikkio\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Zikkio\Http\Behaviors\JsonApiResponses;
 
 abstract class Request extends FormRequest
 {
-    //
+    use JsonApiResponses;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(){
+        return true; //Means authorization is delegated to policies
+    }
+
+    /**
+     * Validation rules for request's parameters
+     * @return array
+     */
+    public function rules(){
+        return [];
+    }
+
+    /**
+    Handles Validation Errors
+     */
+    public function response(array $errors){
+        return $this->respondError('Invalid Request', 'Some fields are missing/invalid');
+    }
+
+    /**
+     * Handles Authorization Errors
+     * @return mixed
+     */
+    public function forbiddenResponse(){
+        return $this->respondForbidden('Action Forbidden', 'You lack the necessary permissions/conditions to perform this action');
+    }
 }
