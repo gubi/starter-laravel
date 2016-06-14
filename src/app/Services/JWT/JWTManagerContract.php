41f06bfbd18068bf2f0d81a85760344aa4b30647
ivcandela <ivankcandela@gmail.com>
2016-06-14 16:54:12 +0200

    Added PHPDoc

--- src/app/Services/JWT/JWTManagerContract.php
+++ src/app/Services/JWT/JWTManagerContract.php
@@ -4,13 +4,30 @@ namespace Zikkio\Services\JWT;
 
 interface JWTManagerContract
 {
-    
+
+    /**
+     * @param array $payload
+     * @return string
+     */
     public function encode($payload);
-    
-    public function decode($token); 
-    
+
+    /**
+     * @param string $token
+     * @return string|null
+     */
+    public function decode($token);
+
+    /**
+     * @param string $token
+     * @return boolean
+     */
     public function isExpired($token);
 
+    /**
+     * @param string $token
+     * @param string $claim
+     * @return mixed
+     */
     public function get($token, $claim);
 
 }
